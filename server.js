9bcf7159ffea2c32e76e63b5ccb14b503b5b3c9e
jonnyparko <parker.jon@gmail.com>
2020-05-09 23:25:50 +0000

    adding still image support

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ var tempFarenheit;
 var humidtyReading;
 const path = require('path');
 var express = require('express')
+const { StillCamera } = require("pi-camera-connect");
+var fs = require('fs')
+
  
 var app = express()
 app.use(express.static('/home/pi/GrowRoomProject/indoor-garden/frontend/startbootstrap-sb-admin-2-gh-pages/'))
+app.use(express.static('/home/pi/GrowRoomProject/backend/'));
+
  app.get('/', function(req, res) {
   res.sendFile('/home/pi/GrowRoomProject/indoor-garden/frontend/startbootstrap-sb-admin-2-gh-pages/index.html');
 })
@@ -28,6 +33,16 @@ app.get('/room', function(req, res) {
   
   res.json(GrowRoomReadings);
 })
+
+
+app.get('/image.jpg', function(req,res){
+  const stillCamera = new StillCamera();
+
+  stillCamera.takeImage().then(image => {
+    fs.writeFileSync("still-image.jpg", image);
+    res.sendFile('/home/pi/GrowRoomProject/backend/still-image.jpg');
+  });
+});
  
 app.listen(3000)
 
